a5eebf1504dc7c42e93debcf925b967dec8f3b4f
YGrylls <910357107@qq.com>
2018-10-30 15:52:33 +0800

    Logo Added

---
+++ src/logo/StyleB.java
@@ -0,0 +1,17 @@
+package logo;
+
+public class StyleB extends BorderStyle{
+
+	@Override
+	public String getPrefix() {
+		// TODO Auto-generated method stub
+		return "(((";
+	}
+
+	@Override
+	public String getPostfix() {
+		// TODO Auto-generated method stub
+		return ")))";
+	}
+	
+}
