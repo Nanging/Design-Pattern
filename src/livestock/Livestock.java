a1b7a4f0905ae7f61268677765f84a6696adc2e9
xiziyi <2656486395@qq.com>
2018-10-31 14:46:26 +0800

    DEMO

--- src/livestock/Livestock.java
+++ src/livestock/Livestock.java
@@ -3,7 +3,7 @@ package livestock;
 import thing.LivestockState;
 
 public abstract class Livestock {
-	protected LivestockState nowstate;
+	public LivestockState nowstate;
 	private int id;
 	//Species enumeration,includiong prices
 	public enum species{
