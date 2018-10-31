1df1c0961583bcd2d1330853923de31d151475b1
fys1997 <2377557041@qq.com>
2018-10-31 13:07:33 +0800

    加入weather测试demo

---
+++ src/WeatherDemo.java
@@ -0,0 +1,28 @@
+import weather.Listvisitor;
+import weather.Weather;
+import weather.Wstate;
+
+import java.util.ArrayList;
+
+public class WeatherDemo {
+	public static void main(String[]args)
+	{
+		ArrayList<Weather>input=new ArrayList<Weather>();
+		input.add(Weather.sunny);
+		input.add(Weather.rainy);
+		input.add(Weather.windy);
+		input.add(Weather.sunny);
+		input.add(Weather.rainy);
+		input.add(Weather.windy);
+		input.add(Weather.windy);
+		input.add(Weather.sunny);
+		input.add(Weather.rainy);
+		int num_states=input.size();
+		Wstate[]state=new Wstate[num_states];
+		for(int i=0;i<num_states;i++)
+			state[i]=Wstate.findandclone(input.get(i));
+		for(int i=0;i<num_states;i++)
+			state[i].work();	
+		Wstate.array.get(0).accept(new Listvisitor());
+	}
+}
