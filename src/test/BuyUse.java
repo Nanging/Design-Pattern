9a43d348c7736d49cfc1eb34059f3d677e4d2891
KiionCC <136438673@qq.com>
2018-11-01 17:39:57 +0800

    完成购买菜单的购买牲畜功能

--- src/test/BuyUse.java
+++ src/test/BuyUse.java
@@ -1,6 +1,5 @@
 package test;
-import buy.BuyOx;
-import buy.ResList;
+import buy.*;
 
 import java.util.Scanner;
 
@@ -13,10 +12,21 @@ public class BuyUse {
 	
 	public void menu(){	
 		ResList resList = ResList.Instance();
-		System.out.println("-------Welcome to buy somthing!-------");
+		System.out.println("-------Welcome to shopping!-------");
 		System.out.println("You have гд" + ResList.Instance().money + " now.");
-		System.out.println("And you have " + resList.l_list.get(0).size() + " ox(en) now.");
-		System.out.println("And you can buy livestocks, seeds or feed here.");
+		System.out.println("And you have " 
+				+ resList.l_list.get(0).size() + " ox(en), "
+				+ resList.l_list.get(1).size() + " sheep, " + 
+				+ resList.l_list.get(2).size() + " chook(s), " + 
+				+ resList.l_list.get(3).size() + " cow(s), " + 
+				+ resList.l_list.get(4).size() + " pig(s), \n" +  
+				+ resList.s_list[0] + " cabbage seed, "
+				+ resList.s_list[1] + " carrot seed, " + 
+				+ resList.s_list[2] + " apple seed, " + 
+				+ resList.s_list[3] + " pear seed, \nand " + 
+				+ resList.s_list[3] + " feed " + "now.");
+		System.out.println("-----------------------------------------------");
+		System.out.println("You can buy livestocks, seeds or feed here.");
 		System.out.println("Enter the follwing number to choose what you want ot buy:");
 		System.out.println("-------Livestocks-------");
 		System.out.println("1. Ox");
@@ -25,16 +35,17 @@ public class BuyUse {
 		System.out.println("4. Cow");
 		System.out.println("5. Pig");
 		System.out.println("-----Seeds or Feed------");
-		System.out.println("6. CabbageSeed");
-		System.out.println("7. CarrotSeed");
-		System.out.println("8. AppleSeed");
-		System.out.println("9. PearSeed");
+		System.out.println("6. Cabbage Seed");
+		System.out.println("7. Carrot Seed");
+		System.out.println("8. Apple Seed");
+		System.out.println("9. Pear Seed");
 		System.out.println("10. Feed");
 		System.out.println("------------------------");
 		System.out.println("11. Cancel");
 		System.out.print("Please enter the nunber:");
 		int flag = 0;
 		int amount = 0;
+		BuyLivestock buyLivestock = null;
 		if(in.hasNextInt())
 			flag = in.nextInt();
 		switch (flag) {
@@ -43,22 +54,45 @@ public class BuyUse {
 			if(in.hasNextInt())
 				amount = in.nextInt();
 			
-			BuyOx buyox = new BuyOx();
-			buyox.start(amount);
+			buyLivestock = new BuyOx();
+			buyLivestock.start(amount);
 			System.out.println("Now you have " + resList.l_list.get(0).size() + " ox(en) now.");
 			break;
 		case 2:
+			System.out.print("Please enter the amount you want to buy:");
+			if(in.hasNextInt())
+				amount = in.nextInt();
 			
+			buyLivestock = new BuySheep();
+			buyLivestock.start(amount);
+			System.out.println("Now you have " + resList.l_list.get(0).size() + " sheep now.");			
 			break;
 		case 3:
+			System.out.print("Please enter the amount you want to buy:");
+			if(in.hasNextInt())
+				amount = in.nextInt();
 			
+			buyLivestock = new BuyChook();
+			buyLivestock.start(amount);
+			System.out.println("Now you have " + resList.l_list.get(0).size() + " chook(s) now.");			
 			break;
-
 		case 4:
+			System.out.print("Please enter the amount you want to buy:");
+			if(in.hasNextInt())
+				amount = in.nextInt();
 			
+			buyLivestock = new BuyCow();
+			buyLivestock.start(amount);
+			System.out.println("Now you have " + resList.l_list.get(0).size() + " cow(s) now.");		
 			break;
 		case 5:
+			System.out.print("Please enter the amount you want to buy:");
+			if(in.hasNextInt())
+				amount = in.nextInt();
 			
+			buyLivestock = new BuyPig();
+			buyLivestock.start(amount);
+			System.out.println("Now you have " + resList.l_list.get(0).size() + " pig(s) now.");		
 			break;
 		case 6:
 
